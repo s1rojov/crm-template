d89fbb432d87dacc9ff2ec8daac4995fce7c5698
s1rojov <sirojoff2727@gmail.com>
2025-03-03 10:11:10 +0500

    fix: some changed & custom table

---
+++ src/components/CustomTable/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="q-pa-md">
+    <q-table flat bordered title="Treats" :rows="items" :columns="fields" row-key="name" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  fields: {
+    type: Array as any,
+    required: true,
+  },
+  items: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
 import '@quasar/extras/material-icons/material-icons.css'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 import App from './App.vue'
 import router from './router'
 
